6501c956e8a98e5dff6c31c04a6d131cde4ea3ee
SpideyGio10 <104469485+SpideyGio10@users.noreply.github.com>
2023-01-05 17:22:28 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAaPBabCaioCatwGRZBW7eFop1UQyA--nc",
+  authDomain: "rabbit-chat-b2af0.firebaseapp.com",
+  projectId: "rabbit-chat-b2af0",
+  storageBucket: "rabbit-chat-b2af0.appspot.com",
+  messagingSenderId: "1068412743071",
+  appId: "1:1068412743071:web:bd9577d68d540029d061aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
